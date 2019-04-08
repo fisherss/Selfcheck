dc71e0e8605a89a7577894fe5b41f797071b8f7f
fisherss <fisherssss@protonmail.com>
2019-04-09 00:38:14 +0800

    diff function finished

--- selfcheck.py
+++ selfcheck.py
@@ -15,16 +15,22 @@ CYELLOWBG = '\33[33m'
 CEND = '\033[0m'
 
 init()
-def check3():
-    org_pd = pd.read_csv('f.csv')
-    new_pd = pd.read_csv('f.tmp')
+def check3(rootdir):
+    diff = pd.DataFrame(columns=['path','org_checksum','new_checksum','org_mtime','new_mtime'])
+    org_pd = pd.read_csv(rootdir + '.csv')
+    new_pd = pd.read_csv(rootdir + '.tmp')
     for index_org, org_row in org_pd.iterrows():
         for index_new, new_row in new_pd.iterrows():
             if (org_row['path'] == new_row['path']):
-                print(org_row['path']);
-                print(new_row['path']);
                 if (org_row['checksum'] != new_row['checksum']) or (org_row['mtime'] != new_row['mtime']):
-                    print("FFFFFound Errror");
+                    print (CRED + org_row['path'] + CEND);
+                    print(org_row['checksum']);
+                    print(new_row['checksum']);
+                    print(org_row['mtime']);
+                    print(new_row['mtime']);
+                    diff = diff.append({'path': org_row['path'],'org_checksum': org_row['checksum'], 'new_checksum': new_row['checksum'],'org_mtime': org_row['mtime'], 'new_mtime': new_row['mtime']},ignore_index=True)
+
+    diff.to_csv(rootdir + '.diff', encoding='utf-8')
 
 def check2():
     with open('f.csv', 'r', encoding='utf-8') as hosts0:
@@ -82,7 +88,7 @@ def build(rootdir, switch):
                 database.to_csv(rootdir + ext, encoding='utf-8')
                 timer = time.time()
     database.to_csv(rootdir + ext, encoding='utf-8')
-    check3()
+    check3(rootdir)
 
 def check(rootdir):
     database = pd.read_csv(rootdir + '.csv')
